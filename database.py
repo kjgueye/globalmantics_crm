68892324920dae47b119dac708ff80c4fe0bf058
Karim Gueye <kjgueye@hotmail.com>
2024-05-21 19:51:21 -0500

    added dollar sign

---
+++ database.py
@@ -0,0 +1,24 @@
+
+class Database:
+
+    def __init__(self, path):
+
+        with open(path, "r") as handle:
+            """
+            import json
+            self.data = json.load(handle)
+            
+
+            import yaml
+            self.data = yaml.safe_load(handle)
+            """
+
+            import xmltodict
+            self.data = xmltodict.parse(handle.read()) ["root"]
+            print(self.data)
+
+    def balance(self, acct_id):
+        acct = self.data.get(acct_id)
+        if acct:
+            return int(acct["due"]) - int(acct["paid"])
+        return None
