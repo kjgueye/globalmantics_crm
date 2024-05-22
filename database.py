565a4f471a39f9590711b98aada82a85655ddfe7
Karim Gueye <kjgueye@hotmail.com>
2024-05-21 19:52:10 -0500

    added dollar sign

--- database.py
+++ database.py
@@ -20,5 +20,6 @@ class Database:
     def balance(self, acct_id):
         acct = self.data.get(acct_id)
         if acct:
-            return int(acct["due"]) - int(acct["paid"])
+            bal = float(acct["due"]) - float(acct["paid"])
+            return f"${bal:.2f}"
         return None
