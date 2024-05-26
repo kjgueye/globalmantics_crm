3c86cd29ac5aa57df3b3990bb1ed6232445360d3
Karim Gueye <kjgueye@hotmail.com>
2024-05-26 07:02:29 -0500

    using USD format

--- database.py
+++ database.py
@@ -8,7 +8,6 @@ class Database:
             import json
             self.data = json.load(handle)
             
-
             import yaml
             self.data = yaml.safe_load(handle)
             """
@@ -21,5 +20,6 @@ class Database:
         acct = self.data.get(acct_id)
         if acct:
             bal = float(acct["due"]) - float(acct["paid"])
-            return f"${bal:.2f}"
+            return f"{bal:.2f} USD"
+            # return f"${bal:.2f}"
         return None
